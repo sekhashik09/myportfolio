c809d91be185453d1d47e84801a06d0c8f023b84
Sekh Ashik Billa <ashiksekh8967@gmail.com>
2024-11-03 01:05:46 +0530

    changes

--- index.js
+++ index.js
@@ -6,18 +6,10 @@ const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
-const allowedOrigins = ['https://yourfrontend.com']; // Update with your frontend domain
+// const allowedOrigins = ['https://yourfrontend.com']; // Update with your frontend domain
 
 // Middleware
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors())
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
